1ff3ce4c80fc190807e8e03f29ac0e78a9653ea7
Amekn <dadaogcd4@gamil.com>
2026-03-04 14:20:16 +1300

    Fix: Update base path for GitHub Pages to /singularity-ai-uow.github.io

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/singularityai.github.io/'
+  base: '/singularity-ai-uow.github.io/'
 })
